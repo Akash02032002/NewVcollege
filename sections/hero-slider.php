a48657a70e3516d37b5eddab2cf13144981145bc
Akash Kumar <aksahkumar02032002@gmail.com>
2026-02-18 04:15:10 +0530

    hero-slider

--- sections/hero-slider.php
+++ sections/hero-slider.php
@@ -9,15 +9,13 @@
           id="mainSlider"
           class="carousel slide"
           data-ride="carousel"
-          data-interval="4000"
-        >
+          data-interval="4000">
           <!-- Indicators -->
           <ol class="carousel-indicators">
             <li
               data-target="#mainSlider"
               data-slide-to="0"
-              class="active"
-            ></li>
+              class="active"></li>
             <li data-target="#mainSlider" data-slide-to="1"></li>
           </ol>
 
@@ -36,24 +34,20 @@
             class="left carousel-control"
             href="#mainSlider"
             role="button"
-            data-slide="prev"
-          >
+            data-slide="prev">
             <span
               class="glyphicon glyphicon-chevron-left"
-              aria-hidden="true"
-            ></span>
+              aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
           </a>
           <a
             class="right carousel-control"
             href="#mainSlider"
             role="button"
-            data-slide="next"
-          >
+            data-slide="next">
             <span
               class="glyphicon glyphicon-chevron-right"
-              aria-hidden="true"
-            ></span>
+              aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
         </div>
@@ -68,8 +62,7 @@
                 <li>
                   <a
                     href="https://topcolleges.co.in/storage/news_pdfs/AuCyQEf2mCHLuvMgPirXcCzmAyQPNAU27tGuyDXJ.pdf"
-                    target="_blank"
-                  >
+                    target="_blank">
                     <i class="fa fa-file-text pdf-icon"></i> Civil
                     Engineering Paper
                   </a>
@@ -77,8 +70,7 @@
                 <li>
                   <a
                     href="https://topcolleges.co.in/storage/news_pdfs/0rzrzZwheY5gjE2j4SeFaCG0EzyiLJR0lMwkq1zI.pdf"
-                    target="_blank"
-                  >
+                    target="_blank">
                     <i class="fa fa-file-text pdf-icon"></i> Top College
                     Highlights – 5/24/2025
                   </a>
@@ -86,8 +78,7 @@
                 <li>
                   <a
                     href="https://topcolleges.co.in/storage/news_pdfs/r7ew3JssfezENFN4XwaO5ZxYVlt5EXNXknaDz4qs.pdf"
-                    target="_blank"
-                  >
+                    target="_blank">
                     <i class="fa fa-file-text pdf-icon"></i> Today's Top
                     College Admissions & Cutoff Updates – [5/24/2025]
                   </a>
@@ -95,8 +86,7 @@
                 <li>
                   <a
                     href="https://topcolleges.co.in/storage/news_pdfs/nzDd56Aac0zDntRSLKlQMc8WSl1nsuFAEbseQGJw.pdf"
-                    target="_blank"
-                  >
+                    target="_blank">
                     <i class="fa fa-file-text pdf-icon"></i> Top 10 Colleges
                     Making Headlines Today
                   </a>
@@ -104,8 +94,7 @@
                 <li>
                   <a
                     href="https://topcolleges.co.in/storage/news_pdfs/lXsVYkFZWuBUOwVWQalRWKofNNICdUwTY0WboTX5.pdf"
-                    target="_blank"
-                  >
+                    target="_blank">
                     <i class="fa fa-file-text pdf-icon"></i> Latest Updates
                     from Top-Ranked Colleges
                   </a>
