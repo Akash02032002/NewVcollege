050f82fa98e471fdb6a7cc6fe103ffd3b69920b9
Akash Kumar <aksahkumar02032002@gmail.com>
2026-02-18 04:23:14 +0530

    new update

--- sections/hero-slider.php
+++ sections/hero-slider.php
@@ -4,104 +4,104 @@
 <div class="slider">
   <div class="slider-layout">
     <div class="slider-main">
-        <div
-          id="mainSlider"
-          class="carousel slide"
-          data-ride="carousel"
-          data-interval="4000">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li
-              data-target="#mainSlider"
-              data-slide-to="0"
-              class="active"></li>
-            <li data-target="#mainSlider" data-slide-to="1"></li>
-          </ol>
+      <div
+        id="mainSlider"
+        class="carousel slide"
+        data-ride="carousel"
+        data-interval="4000">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li
+            data-target="#mainSlider"
+            data-slide-to="0"
+            class="active"></li>
+          <li data-target="#mainSlider" data-slide-to="1"></li>
+        </ol>
 
-          <!-- Slides -->
-          <div class="carousel-inner" role="listbox">
-            <div class="item active">
-              <img src="images/slider2.webp" alt="Slider 1" />
-            </div>
-            <div class="item">
-              <img src="images/slider.webp" alt="Slider 2" />
-            </div>
+        <!-- Slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="images/slider2.webp" alt="Slider 1" />
           </div>
-
-          <!-- Controls -->
-          <a
-            class="left carousel-control"
-            href="#mainSlider"
-            role="button"
-            data-slide="prev">
-            <span
-              class="glyphicon glyphicon-chevron-left"
-              aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a
-            class="right carousel-control"
-            href="#mainSlider"
-            role="button"
-            data-slide="next">
-            <span
-              class="glyphicon glyphicon-chevron-right"
-              aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
+          <div class="item">
+            <img src="images/slider.webp" alt="Slider 2" />
+          </div>
         </div>
+
+        <!-- Controls -->
+        <a
+          class="left carousel-control"
+          href="#mainSlider"
+          role="button"
+          data-slide="prev">
+          <span
+            class="glyphicon glyphicon-chevron-left"
+            aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a
+          class="right carousel-control"
+          href="#mainSlider"
+          role="button"
+          data-slide="next">
+          <span
+            class="glyphicon glyphicon-chevron-right"
+            aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
     </div>
 
     <div class="slider-sidebar">
-        <div class="updates-box">
-          <div class="updates-header">DAILY UPDATES</div>
-          <div class="updates-content-wrapper">
-            <div class="updates-content">
-              <ul>
-                <li>
-                  <a
-                    href="https://topcolleges.co.in/storage/news_pdfs/AuCyQEf2mCHLuvMgPirXcCzmAyQPNAU27tGuyDXJ.pdf"
-                    target="_blank">
-                    <i class="fa fa-file-text pdf-icon"></i> Civil
-                    Engineering Paper
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://topcolleges.co.in/storage/news_pdfs/0rzrzZwheY5gjE2j4SeFaCG0EzyiLJR0lMwkq1zI.pdf"
-                    target="_blank">
-                    <i class="fa fa-file-text pdf-icon"></i> Top College
-                    Highlights – 5/24/2025
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://topcolleges.co.in/storage/news_pdfs/r7ew3JssfezENFN4XwaO5ZxYVlt5EXNXknaDz4qs.pdf"
-                    target="_blank">
-                    <i class="fa fa-file-text pdf-icon"></i> Today's Top
-                    College Admissions & Cutoff Updates – [5/24/2025]
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://topcolleges.co.in/storage/news_pdfs/nzDd56Aac0zDntRSLKlQMc8WSl1nsuFAEbseQGJw.pdf"
-                    target="_blank">
-                    <i class="fa fa-file-text pdf-icon"></i> Top 10 Colleges
-                    Making Headlines Today
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://topcolleges.co.in/storage/news_pdfs/lXsVYkFZWuBUOwVWQalRWKofNNICdUwTY0WboTX5.pdf"
-                    target="_blank">
-                    <i class="fa fa-file-text pdf-icon"></i> Latest Updates
-                    from Top-Ranked Colleges
-                  </a>
-                </li>
-              </ul>
-            </div>
+      <div class="updates-box">
+        <div class="updates-header">DAILY UPDATES</div>
+        <div class="updates-content-wrapper">
+          <div class="updates-content">
+            <ul>
+              <li>
+                <a
+                  href="https://topcolleges.co.in/storage/news_pdfs/AuCyQEf2mCHLuvMgPirXcCzmAyQPNAU27tGuyDXJ.pdf"
+                  target="_blank">
+                  <i class="fa fa-file-text pdf-icon"></i> Civil
+                  Engineering Paper
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://topcolleges.co.in/storage/news_pdfs/0rzrzZwheY5gjE2j4SeFaCG0EzyiLJR0lMwkq1zI.pdf"
+                  target="_blank">
+                  <i class="fa fa-file-text pdf-icon"></i> Top College
+                  Highlights – 5/24/2025
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://topcolleges.co.in/storage/news_pdfs/r7ew3JssfezENFN4XwaO5ZxYVlt5EXNXknaDz4qs.pdf"
+                  target="_blank">
+                  <i class="fa fa-file-text pdf-icon"></i> Today's Top
+                  College Admissions & Cutoff Updates – [5/24/2025]
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://topcolleges.co.in/storage/news_pdfs/nzDd56Aac0zDntRSLKlQMc8WSl1nsuFAEbseQGJw.pdf"
+                  target="_blank">
+                  <i class="fa fa-file-text pdf-icon"></i> Top 10 Colleges
+                  Making Headlines Today
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://topcolleges.co.in/storage/news_pdfs/lXsVYkFZWuBUOwVWQalRWKofNNICdUwTY0WboTX5.pdf"
+                  target="_blank">
+                  <i class="fa fa-file-text pdf-icon"></i> Latest Updates
+                  from Top-Ranked Colleges
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
+      </div>
     </div>
   </div>
 </div>
